9e0c29afacdb4534b10ccd0bf06b595ea1ce7827
cristian-glab <cristian.pinzon@gracialab.com.co>
2024-03-26 16:23:34 -0500

    Add access control origin  and cors additional res headers

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,10 @@ async function bootstrap() {
     origin: '*'
   })
 
-  app.use(function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
   });
 
